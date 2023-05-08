36e38195725f843c1f4137fbd982fdbb0fc88f74
AkihiroTanaka3 <qqqwwetyu@gmail.com>
2023-05-08 14:59:44 +0000

    制作途中

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,13 +1,47 @@
 class Admin::MoviesController < ApplicationController
+
   def index
+      if params[:looking_for]
+        movie_title = params[:looking_for]
+        movies = []
+        (1..5).each do |page|
+          url = "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_API']}&language=ja&query=" + URI.encode_www_form_component(movie_title) + "&page=#{page}"
+          response = Net::HTTP.get_response(URI.parse(url))
+          if response.code == "200"
+            result = JSON.parse(response.body)
+            movies.concat(result["results"])
+          end
+        end
+      else
+        movies = []
+        (1..5).each do |page|
+          url = "https://api.themoviedb.org/3/movie/popular?api_key=#{ENV['TMDB_API']}&language=ja&page=#{page}"
+          response = Net::HTTP.get_response(URI.parse(url))
+          if response.code == "200"
+            result = JSON.parse(response.body)
+            movies.concat(result["results"])
+          end
+        end
+      end
+      @movies = Kaminari.paginate_array(movies).page(params[:page]).per(20)
   end
 
   def show
+    movie_id = params[:id]
+    url = "https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV['TMDB_API']}&language=ja"
+    @movie = JSON.parse(Net::HTTP.get(URI.parse(url)))
   end
 
-  def new
-  end
-
-  def edit
+  def create
+    movie = params[:movie]
+    Movie.new(
+      name: movie['title'],
+      year: movie['release_date'],
+      genre_name: movie["genres"][0]["name"],
+      image: movie['poster_path'],
+      overview: movie['overview'],
+      )
+      .save
+      redirect to movies_path
   end
 end
--- db/migrate/20230504122050_create_movies.rb
+++ db/migrate/20230504122050_create_movies.rb
@@ -3,10 +3,12 @@ class CreateMovies < ActiveRecord::Migration[6.1]
     create_table :movies do |t|
       t.string   :name
       t.integer  :read_count, null: false, default: 0
-      t.integer  :cast_id
+      # t.integer  :cast_id
       t.string   :year
       t.text     :overview
-      t.integer  :genre_id 
+      # t.integer  :genre_id 
+      t.string   :genre_name
+      t.string   :image
 
       t.timestamps
     end
