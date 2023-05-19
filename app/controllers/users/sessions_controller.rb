efdb5874cc9f642a9bb0681344ce3bc7e6581e99
AkihiroTanaka3 <qqqwwetyu@gmail.com>
2023-05-19 06:37:34 +0000

    編集

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,10 +4,15 @@ class Users::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
   before_action :reject_withdraw_user, only: [:create]
   def guest_sign_in
-    user = User.guest
-    sign_in user
-    redirect_to movies_path, notice: 'guestuserでログインしました。'
+    if user_signed_in?
+      redirect_to movies_path, notice: '既にログインしています。'
+    else
+      user = User.guest
+      sign_in user
+      redirect_to movies_path, notice: 'guestuserでログインしました。'
+    end
   end
+
   # GET /resource/sign_in
   # def new
   #   super
@@ -32,7 +37,7 @@ class Users::SessionsController < Devise::SessionsController
   def reject_withdraw_user
     @user = User.find_by(email: params[:user][:email])
     return if !@user
-      if @user.valid_password?(params[:user][:password]) && @user.membership_status == true
+    if @user.valid_password?(params[:user][:password]) && @user.membership_status == true
       flash[:notice] = "退会済みの為、再登録が必要です。"
       redirect_to new_user_registration_path
     end
