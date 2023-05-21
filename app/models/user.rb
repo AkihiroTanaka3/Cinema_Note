b6add89cbf36daa9ecaa73fbc48655a936ce2200
AkihiroTanaka3 <qqqwwetyu@gmail.com>
2023-05-21 14:46:08 +0000

    モデル編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,24 +8,24 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :liked_movies, through: :favorites, source: :movie
 
-  validates :name, presence: true, length: {maximum: 10}
-  validates :introduction, length: {maximum: 50 }
-  
+  validates :name, presence: true, length: {maximum: 10}, uniqueness: true
+  validates :introduction, length: {maximum: 20 }
+
   def self.guest
     find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "guestuser"
     end
   end
-  
+
   def guest?
     email == 'guest@example.com'
   end
-  
+
   def likes?(movie)
     self.favorites.exists?(movie_id: movie.id)
   end
-  
+
   def admin?
     admin # モデルの属性として定義されている場合、そのまま返す
   end
